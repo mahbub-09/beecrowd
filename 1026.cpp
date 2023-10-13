fd72a222729857b4d3b6317b258eddfbb67099f3
mahbub-09 <mahbubrahman939@gmail.com>
2023-10-13 12:35:23 +0600

    one more item

---
+++ 1026.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n1, n2;
+
+    while(cin >> n1 >> n2){
+        cout << (n1^n2) << endl;
+    }
+
+
+
+
+    return 0;
+}
