3d798b446ca2cd77aeda2b6f98d3b8fcc1695be1
mahbub-09 <mahbubrahman939@gmail.com>
2023-10-13 12:57:58 +0600

    accepted solution

--- 2556.cpp
+++ 2556.cpp
@@ -23,33 +23,3 @@ int main()
     return 0;
 }
 
-//        int arr[n], mn = INT_MAX, mx = INT_MIN, mx_idx, mn_idx;
-//        for(int i = 0; i < n; ++i){
-//            cin >> arr[i];
-//
-//            if(mn > arr[i]){
-//                mn = arr[i];
-//                mn_idx = i;
-//            }
-//
-//            if(mx < arr[i]){
-//                mx = arr[i];
-//                mx_idx = i;
-//            }
-//        }
-//
-//        arr[mn_idx] = 0;
-//        arr[mx_idx] = 0;
-//        mn = INT_MAX;
-//        mx = INT_MIN;
-//
-//        for(int i = 0; i < n; ++i){
-//            for(int j = i+1; j < n-1; ++j){
-//                if(arr[i] < arr[j]){
-//                    swap(arr[i], arr[j]);
-//                }
-//            }
-//        }
-//
-//        cout << (n/2) << " " << abs(arr[0] - arr[n-3]) << "\n";
-//    }
